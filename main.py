b24cc9188d6d73e80741eb55ec61a51ab1f7581f
Siddhartha Kolla <siddharthansg@gmail.com>
2024-01-09 21:54:07 +0100

    New Log

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import random
 
 # Player board
 board_len = 10
-board = [["" for i in range(board_len)] for x in range(board_len)]
 
+board = [["" for i in range(board_len)] for x in range(board_len)]
 com_board = [["" for i in range(board_len)] for x in range(board_len)]
 
-# Table to print the players board
-header = [chr(65+i) for i in range(board_len)]
-table = tabulate(board,tablefmt="rounded_grid",showindex=True,headers=header)
-print(table)
+def create_table(board_len,data):
+    
+    # Table to print the players board
+    header = [chr(65+i) for i in range(board_len)]
+    board_index = range(1,board_len+1)
+    table = tabulate(data,tablefmt="rounded_grid",showindex=board_index,headers=header)
+    return table
 
-
-
+# def place_ships(com_board):
+    
 for i in range(2,6):
+
     pos_ok = False
     while not pos_ok:
+        
         pos = (random.randrange(board_len),random.randrange(board_len))
         orientation = random.choice(["vertical","horizontal"])
         if (pos[0]+i) % board_len == 1 or (pos[1]+i) % board_len == 1:
@@ -41,6 +46,7 @@ for i in range(2,6):
         else:
             temp_status = False
             for x in range(i):
+                
                 if pos[0]+x >= board_len:
                     temp_status = not temp_status
                     break
@@ -55,7 +61,11 @@ for i in range(2,6):
                 pos_ok = True
                 for x in range(i):
                     com_board[pos[0]+x][pos[1]] = f"{i}"
-# Table to print the players board
-header = [chr(65+i) for i in range(board_len)]
-table = tabulate(com_board,tablefmt="rounded_grid",showindex=True,headers=header)
-print(table)
+
+
+# com_board = place_ships(com_board)
+print(create_table(board_len,board))
+print(create_table(board_len,com_board))
+
+
+print("Welcome to the Python Battleship Game\n\n")
