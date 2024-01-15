ac9f831f3b04731bf42cba081f005f11c6877290
Siddhartha Kolla <siddharthansg@gmail.com>
2024-01-15 22:09:45 +0100

    Player can now edit the board using the place_ships function

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from tabulate import tabulate
 import random
 import os
 
-# Player board
-global board_len
-board_len = 10
-
-board = [["" for i in range(board_len)] for x in range(board_len)]
-com_board = [["" for i in range(board_len)] for x in range(board_len)]
 
 def create_table(board_len,data):
     
@@ -17,81 +11,88 @@ def create_table(board_len,data):
     table = tabulate(data,tablefmt="rounded_grid",showindex=board_index,headers=header)
     return table
 
-# def place_ships(com_board):
-    
-for i in range(2,6):
+def ask_user_input(board_len, message,board,com_board):
+    print(tabulate([[create_table(board_len,board), create_table(board_len,com_board)]], rowalign="center", tablefmt="grid"))
+    user_input = input(message).strip()
+    while not(int(user_input[1:])-1 < board_len and ord(user_input[0].upper())-65 < board_len):
+        user_input = input("Incorrect Coordinate!! Please type in your coordinate like H7: ").strip()
+    return [int(user_input[1:])-1,ord(user_input[0].upper())-65]
 
-    pos_ok = False
-    while not pos_ok:
-        
-        pos = (random.randrange(board_len),random.randrange(board_len))
-        orientation = random.choice(["vertical","horizontal"])
-        if (pos[0]+i) % board_len == 1 or (pos[1]+i) % board_len == 1:
-            continue
-        if orientation == "horizontal":
-            temp_status = False
-            for x in range(i):
-                if pos[1]+x >= board_len:
-                    temp_status = False
-                    break
-                if com_board[pos[0]][pos[1]+x] == "":
-                    temp_status = True
-                else:
-                    temp_status = False
-                    break
-            if temp_status == False:
+def place_ships(com_board,board_len,board):
+
+    for i in range(2,6):
+
+        pos_ok = False
+        while not pos_ok:
+            os.system("clear")
+            pos = ask_user_input(board_len,f"You should now place a Ship with a size of {i}. Please choose the starting pos of the ship: ",board,com_board)
+            orientation = input("vertical or horizontal?: ")
+            while orientation.lower() != "vertical" and orientation.lower() != "horizontal":
+                orientation = input("Invalid argument. vertical or horizontal?: ")
+            if (pos[0]+i) % board_len == 1 or (pos[1]+i) % board_len == 1:
                 continue
-            else:
-                pos_ok = True
+            if orientation == "horizontal":
+                temp_status = False
                 for x in range(i):
-                    com_board[pos[0]][pos[1]+x] = f"{i}"
-        else:
-            temp_status = False
-            for x in range(i):
-                
-                if pos[0]+x >= board_len:
-                    temp_status = not temp_status
-                    break
-                if com_board[pos[0]+x][pos[1]] == "":
-                    temp_status = True
+                    if pos[1]+x >= board_len:
+                        temp_status = False
+                        break
+                    if com_board[pos[0]][pos[1]+x] == "":
+                        temp_status = True
+                    else:
+                        temp_status = False
+                        break
+                if temp_status == False:
+                    continue
                 else:
-                    temp_status = False
-                    break
-            if temp_status == False:
-                continue
+                    pos_ok = True
+                    for x in range(i):
+                        com_board[pos[0]][pos[1]+x] = f"{i}"
             else:
-                pos_ok = True
+                temp_status = False
                 for x in range(i):
-                    com_board[pos[0]+x][pos[1]] = f"{i}"
-
-
-# com_board = place_ships(com_board)
-# print(create_table(board_len,board))
-os.system("clear")
-print("=============================Welcome to the Python Battleship Game=============================\n\n")
+                    
+                    if pos[0]+x >= board_len:
+                        temp_status = not temp_status
+                        break
+                    if com_board[pos[0]+x][pos[1]] == "":
+                        temp_status = True
+                    else:
+                        temp_status = False
+                        break
+                if temp_status == False:
+                    continue
+                else:
+                    pos_ok = True
+                    for x in range(i):
+                        com_board[pos[0]+x][pos[1]] = f"{i}"
+    return com_board
 
 def if_sunk_or_not(com_board,num):
     return any(num in x for x in com_board)
 
-def ask_user_input(board_len):
-    print(create_table(board_len,board))
-    user_input = input("Please type in your coordinate like H7: ").strip()
-    while not(int(user_input[1:])-1 < board_len and ord(user_input[0].upper())-65 < board_len):
-        user_input = input("Incorrect Coordinate!! Please type in your coordinate like H7: ").strip()
-    return [int(user_input[1:])-1,ord(user_input[0].upper())-65]
+os.system("clear")
+print("=============================Welcome to the Python Battleship Game=============================\n\n")
+
+
 
 
 def game_loop(board_len):
     sunk_ships = 0
     last_turn = 30
     turns = 0
+    # Player board
+    board_len = 10
+    board = [["" for i in range(board_len)] for x in range(board_len)]
+    com_board = [["" for i in range(board_len)] for x in range(board_len)]
+    com_board = place_ships(com_board,board_len,board)
     while sunk_ships != 4 and turns <= last_turn:
         turns += 1
-        ui = ask_user_input(board_len)
+        ui = ask_user_input(board_len,"Please type in your coordinate like H7: ",board,com_board)
         while board[ui[0]][ui[1]] == "-" or board[ui[0]][ui[1]] == "X":
             os.system("clear")
             print("You already guessed this coordinate")
-            ui = ask_user_input(board_len)
+            ui = ask_user_input(board_len,"Please type in your coordinate like H7: ",board,com_board)
         if com_board[ui[0]][ui[1]].isdigit():
             os.system("clear")
             print(f"{chr(65+ui[1])}{ui[0]+1} is a Hit\n")
@@ -114,4 +115,4 @@ def game_loop(board_len):
             
 
 
-game_loop(board_len)
+game_loop(10)
